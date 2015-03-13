4657bf5a7781e89b1f0f17cd031c1b38fca034be
R. Tony Goold <tony@wattpad.com>
2015-03-12 20:59:05 -0400

    Added a basic data model.

--- Person List/ViewController.swift
+++ Person List/ViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var people = Array<Person>()
+
+    override func viewDidLoad() {
+        people.append(Person(name: "Alice"))
+        people.append(Person(name: "Bob"))
+        people.append(Person(name: "Charlie"))
+    }
+
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return people.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        // Step 1: Get the Person for the row
+        let person = people[indexPath.row]
+
+        // Step 2: Create and configure the row for the Person
+        var cell = UITableViewCell()
+        cell.textLabel?.text = person.name
+        return cell
     }
 
 }
