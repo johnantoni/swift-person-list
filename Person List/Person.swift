4be0c6421148eae3360af50329c325f402933007
R. Tony Goold <tony@wattpad.com>
2015-03-12 20:52:21 -0400

    Added a table view with minimum implementation.

---
+++ Person List/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  SingleView
+//
+//  Created by R. Tony Goold on 2015-03-12.
+//  Copyright (c) 2015 Tony. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    var name: String
+    var jobTitle: String?
+    var address: String?
+
+    init(name: String) {
+        self.name = name
+    }
+}
--- Person List/ViewController.swift
+++ Person List/ViewController.swift
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-
 
 }
 
